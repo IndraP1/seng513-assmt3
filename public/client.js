4ce87656b5db799b724b20dca18503090f6a0d5f
indra prasetyo <indra.prasetyo@calgaryscientific.com>
2017-03-09 00:58:54 -0700

    Colors and name changing working

--- public/client.js
+++ public/client.js
@@ -12,6 +12,17 @@ $(function() {
         var current_date = new Date(); 
 
         // if /nick tag or /color tag perform other operations else:
+        // var firstWord = msg.substr(0, msg.indexOf(" "));
+        var firstWord = msg.split(' ')[0];
+
+        if (firstWord === "/nick") {
+            var secondWord = msg.split(' ')[1];
+            setCookie("username", secondWord);
+        } else if (firstWord === "/nickcolor") {
+            var secondWord = msg.split(' ')[1];
+            setCookie("usercolor", secondWord);
+        }
+
         var current_time = current_date.getHours() + ":"
             + current_date.getMinutes() + " ";
         var username = getCookie("username");
@@ -23,12 +34,19 @@ $(function() {
     });
 });
 
+function getFirstWord(str) {
+    if (str.indexOf(' ') === -1)
+        return -1;
+    else
+        return str.substr(0, str.indexOf(' '));
+};
+
 // Code taken from https://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    // var d = new Date();
+   // d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    // var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";path=/";
 }
 
 function getCookie(cname) {
